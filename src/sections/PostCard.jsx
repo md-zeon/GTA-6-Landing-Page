4170202d3c2e572cc103aba630ef52671f442c5f
md-zeon <zeon.cse@gmail.com>
2025-11-25 12:40:50 +0600

    Add PostCard section with GSAP animations and integrate into App component

---
+++ src/sections/PostCard.jsx
@@ -0,0 +1,56 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { useRef } from "react";
+
+const PostCard = () => {
+	const videoRef = useRef(null);
+
+	useGSAP(() => {
+		const tl = gsap.timeline({
+			scrollTrigger: {
+				trigger: ".post-card", // Section trigger for the animation
+				start: "top center", // Animation starts when the top of the section hits the center of the viewport
+				end: "bottom center", // Animation ends when the bottom of the section hits the center of the viewport
+				scrub: true, // Smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+			},
+		});
+
+		videoRef.current.onloadedmetadata = () => {
+			tl.to(
+				videoRef.current,
+				{
+					currentTime: videoRef.current.duration,
+					duration: 3,
+					ease: "power1.inOut",
+				},
+				"<",
+			);
+		};
+	});
+	return (
+		<section className='post-card'>
+			<div className='animated-gradient-bg' />
+
+			<div className='post-card-wrapper group hover:rotate-1 hover:scale-[1.02] transition duration-700'>
+				<img
+					src='/images/overlay.webp'
+					alt='Overlay Image'
+				/>
+				<video
+					ref={videoRef}
+					muted
+					playsInline
+					preload='auto'
+					autoPlay
+					src='/videos/postcard-vd.mp4'
+				/>
+			</div>
+
+			<button className='group-hover:bg-yellow transition duration-700'>
+				Explore Leonida Keys
+			</button>
+		</section>
+	);
+};
+
+export default PostCard;
